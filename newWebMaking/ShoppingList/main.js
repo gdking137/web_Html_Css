ffa6cdd8d21d6f420cf1ec867efcdf991c91c8ae
jkim8 <80569681+gdking137@users.noreply.github.com>
2022-08-22 20:53:24 -0400

    added a working function to the website
    now the buttons to add delete and push exists to make the website look like a shopping list

---
+++ newWebMaking/ShoppingList/main.js
@@ -0,0 +1,53 @@
+const items = document.querySelector(".items");
+const input = document.querySelector(".footer_input");
+const addBtn = document.querySelector(".footer_button");
+
+function onAdd() {
+  const text = input.value;
+  if (text === "") {
+    input.focus();
+    return;
+  }
+
+  const item = createItem(text);
+
+  items.appendChild(item);
+
+  input.value = "";
+  input.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.document.createElement("li");
+  itemRow.setAttribute("class", "item_row");
+
+  const item = document.createElement("div");
+  item.setAttribute("class", "item");
+
+  const name = document.createElement("span");
+  name.setAttribute("class", "item_name");
+  name.innerText = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("class", "item_delete");
+  deleteBtn.innerHTML = '<i class= "fas fa-trash-alt"></i>';
+  deleteBtn.addEventListener("click", () => {
+    items.removeChild(itemRow);
+  });
+  const itemDivider = document.createElement("div");
+  itemDivider.setAttribute("class", "item_Divider");
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+}
+
+addBtn.addEventListener("click", () => {
+  onAdd();
+});
+
+input.addEventListener("keypress", (event) => {
+  console.log("key");
+});
