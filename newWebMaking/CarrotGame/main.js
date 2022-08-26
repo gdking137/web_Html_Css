1979078a3d3eced3a927529e50553a2d420cedd7
gdking137 <80569681+gdking137@users.noreply.github.com>
2022-08-26 00:26:43 -0400

    some error with the css and added a starting a game.
    the game can now start. When the game starts there is various function that is applied to make the game work. In this push, I've added game stop, timer, score, and start initiation when the game starts.

--- newWebMaking/CarrotGame/main.js
+++ newWebMaking/CarrotGame/main.js
@@ -1,12 +1,50 @@
 const { isMetaProperty } = require("typescript");
 const Carrot_size = 80;
+const Carrot_count = 5;
+const Bug_count = 5;
+
 const field = document.querySelector(".game_field");
 const fieldRect = field.getBoundingClientRect();
-//size position
+const gameBtn = document.querySelector(".game_button");
+const gameTimer = document.querySelector(".game_timer");
+const gameScore = document.querySelector(".game_score");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initgame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
 
 function initgame() {
   //bug+carrot spawn and place them into the field
-  console.log(fieldRect);
+  field.innerHTML = "";
+  gameScore.innerText = Carrot_count;
   addItem("carrot", 5, "img/carrot.png");
   addItem("bug", 5, "img/bug.png");
 }
